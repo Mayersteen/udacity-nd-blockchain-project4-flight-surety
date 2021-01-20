40b09f43c388392c88bd6f4cfdadc5035d5e490e
Mayersteen <mayersteen@gmx.de>
2021-01-20 22:56:06 +0100

    Add development network that doesn't use the HDWalletProvider to avoid potential impact in tests.

--- truffle-config.js
+++ truffle-config.js
@@ -4,6 +4,12 @@ var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble s
 module.exports = {
   networks: {
     development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 9999999
+    },
+    development_hd_wallet: {
       provider: function() {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
